a072936d19f0c0f3b2a6100ebfeab59a6603af3a
Rama Krishna <ramakrishnaprasadvuppala@gmail.com>
2023-02-01 08:34:48 +0000

    Hello World programme

---
+++ hello_world.c
@@ -0,0 +1,10 @@
+/*
+ * This is my first programme
+ */
+
+#include <stdio.h> /* Here all the standard libraries are called from headers */
+int main()
+{
+	printf("hello\tworld\n"); /*printf is standard input output library where l;ogic is implemented in header stdio*/
+	return 0;
+}
